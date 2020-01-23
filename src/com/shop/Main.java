d155e388b57b6a3fecabac3b6c9837f0285e4717
Bistika <48261571+Bistika@users.noreply.github.com>
2020-01-23 18:20:16 +0200

    float formatter created

---
+++ src/com/shop/Main.java
@@ -0,0 +1,18 @@
+package com.shop;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scanner  = new Scanner(System.in);
+    System.out.println("Please insert a float you want to round to 2 decimals:_");
+    if (scanner.hasNextFloat()){
+        float number = scanner.nextFloat();
+        System.out.println(String.format("%.2f", number));
+        }
+    else {
+        System.out.println("The input is not float.");
+        }
+    }
+}
